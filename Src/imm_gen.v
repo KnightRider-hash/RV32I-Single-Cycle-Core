c36aa18b28dfc3503f31f444ded662f3668222d7
Gopanandhan PD <gopanandhanpd05@gmail.com>
2026-07-04 23:30:11 +0530

    Update imm_gen.v
    Implement format-based instruction decoding via instr[3].

--- Src/imm_gen.v
+++ Src/imm_gen.v
@@ -27,14 +27,15 @@ module imm_gen(
     );
     always@(*)begin
 case(immsrc)
-2'b00 : result={{20{instr[31]}},instr[31:20]};
-2'b01 : result={{20{instr[31]}},instr[31:25],instr[11:7]};
-2'b10 : result={{20{instr[31]}},instr[7],instr[30:25],instr[11:8],1'b0};
+2'b00 : result={{20{instr[31]}},instr[31:20]};//i type
+2'b01 : result={{20{instr[31]}},instr[31:25],instr[11:7]}; //s type
+2'b10 : result={{20{instr[31]}},instr[7],instr[30:25],instr[11:8],1'b0}; // b type
 2'b11 : begin 
-     if(instr[6:0]==7'b0110111)
-       result={instr[31:12],{12{1'b0}}};
+     if(instr[3]==1'b0 )
+       result={instr[31:12],{12{1'b0}}};//lui,auipc
      else
-    result={{12{instr[31]}},instr[19:12],instr[20],instr[30:21],1'b0};
+       result={{12{instr[31]}},instr[19:12],instr[20],instr[30:21],1'b0};//jal
+   
     end
 default : result=32'b0;
 endcase
