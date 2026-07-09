64ee6d370f5dda67c3ee34daf85237311c3f43ef
Gopanandhan PD <gopanandhanpd05@gmail.com>
2026-07-09 22:54:25 +0530

    Update ram.v
    added masking feature

--- Src/ram.v
+++ Src/ram.v
@@ -23,7 +23,7 @@
 module ram(
 input [31:0] dain,
 input memrd,
-input memwr,
+input [3:0] mask,
 input clk,
 input [31:0] adr,
 output reg [31:0] daout);
@@ -33,13 +33,23 @@ reg [31:0] store [0:31];
 always@(*)
  begin
 if(memrd==1) 
-   daout<=store[adr>>2];
+   daout=store[adr>>2]; // no need for waiting as blocking assignment used
 else
-   daout<=32'b0;
+   daout=32'b0;
 end
 
 always @(posedge clk) begin 
- if(memwr==1)
-  store[adr>>2]<=dain;
- end 
+ if(mask[0]==1) 
+ store[adr>>2][7:0]<=dain[7:0]; // for safely keep the data using same width on both side or data corruption
+
+ if(mask[1]==1) 
+ store[adr>>2][15:8]<=dain[15:8];
+
+  if(mask[2]==1) 
+ store[adr>>2][23:16]<=dain[23:16];
+
+  if(mask[3]==1) 
+ store[adr>>2][31:24]<=dain[31:24];
+  
+end 
 endmodule
